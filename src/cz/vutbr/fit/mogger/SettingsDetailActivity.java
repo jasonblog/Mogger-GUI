beb75fdb2d743519e6ce9cc16c398cafb5f6ec8f
Irteam <jakub.adamek@netapes.cz>
2014-11-30 15:56:04 +0100

    MP3

--- src/cz/vutbr/fit/mogger/SettingsDetailActivity.java
+++ src/cz/vutbr/fit/mogger/SettingsDetailActivity.java
@@ -28,6 +28,9 @@ public class SettingsDetailActivity extends Activity {
     TextView gestureOk = null;
     ImageButton save = null;
     ImageButton delete = null;
+
+    String filePath = null;
+    String fileOnlyName = null;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,7 +41,11 @@ public class SettingsDetailActivity extends Activity {
         fileDialog.setFileEndsWith(".mp3");
         fileDialog.addFileListener(new cz.vutbr.fit.mogger.FileDialog.FileSelectedListener() {
             public void fileSelected(File file) {
-                Log.d(getClass().getName(), "selected file " + file.toString());
+                filePath = file.toString();
+                int index = filePath.lastIndexOf('\\');
+                fileOnlyName = filePath.substring(index, filePath.length());
+                fileName.setText(fileOnlyName);
+                //Log.d(getClass().getName(), "selected file " + file.toString());
             }
         });
 
@@ -57,12 +64,5 @@ public class SettingsDetailActivity extends Activity {
         gestureOk = (TextView)findViewById(R.id.textView7);
         save = (ImageButton)findViewById(R.id.imageButton5);
         delete = (ImageButton)findViewById(R.id.imageButton4);
-    }
-
-    @Override
-    public void onResume()
-    {
-        super.onResume();
-        fileName.setText(mPath.getName());
     }
 }
