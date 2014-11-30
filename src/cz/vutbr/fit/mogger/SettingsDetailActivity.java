4078300f912c6c1eeab50df3f3d7dd129b0afc90
murrysoft <murry@seznam.cz>
2014-11-30 14:36:07 +0100

    file manager

--- src/cz/vutbr/fit/mogger/SettingsDetailActivity.java
+++ src/cz/vutbr/fit/mogger/SettingsDetailActivity.java
@@ -1,18 +1,16 @@
 package cz.vutbr.fit.mogger;
-
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import cz.vutbr.fit.mogger.R;
-
 /**
  * Created by murry on 29.11.14.
  */
-
-//asdgdhsdfdfgjhfhjjhfj
 public class SettingsDetailActivity extends Activity {
     EditText name = null;
     SeekBar threshold = null;
@@ -22,21 +20,26 @@ public class SettingsDetailActivity extends Activity {
     TextView gestureOk = null;
     ImageButton save = null;
     ImageButton delete = null;
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settingsdetailactivity);
-        //check all
         name = (EditText)findViewById(R.id.editText);
         threshold = (SeekBar)findViewById(R.id.seekBar);
         openFile = (ImageButton)findViewById(R.id.imageButton);
+        openFile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+// settings
+                Intent myIntent = new Intent(SettingsDetailActivity.this, cz.vutbr.fit.mogger.FileBrowser.class);
+                startActivity(myIntent);
+            }
+        });
         fileName = (TextView)findViewById(R.id.textView5);
         addGesture = (ImageButton)findViewById(R.id.imageButton2);
         gestureOk = (TextView)findViewById(R.id.textView7);
         save = (ImageButton)findViewById(R.id.imageButton5);
         delete = (ImageButton)findViewById(R.id.imageButton4);
-
         if(getIntent().getSerializableExtra("Gesture") == "ahoj")
         {
             name.setText("Enter name...");
